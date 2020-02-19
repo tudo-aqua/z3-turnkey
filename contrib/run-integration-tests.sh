b81df385f71726e0d0708072225f0492dbe83427
Simon Dierl <simon.dierl@cs.tu-dortmund.de>
2020-02-19 16:34:25 +0100

    fix jabba download on windows

--- contrib/run-integration-tests.sh
+++ contrib/run-integration-tests.sh
@@ -3,21 +3,47 @@
 set -euxo pipefail
 shopt -s extglob
 
+ls -aR # Debug output
+
+# Install Jabba
+
+JABBA_VERSION="$(curl -Ls https://shyiko.github.com/jabba/latest)"
+case "${OS_IMAGE}" in
+ubuntu-latest)
+  JABBA_SUFFIX='linux-amd64'
+  ;;
+macOS-latest)
+  JABBA_SUFFIX='darwin-amd64'
+  ;;
+windows-latest)
+  JABBA_SUFFIX='windows-amd64.exe'
+  ;;
+esac
+JABBA_GITHUB="https://github.com/shyiko/jabba/releases/download"
+curl -L "${JABBA_GITHUB}/${JABBA_VERSION}/jabba-${JABBA_VERSION}-${JABBA_SUFFIX}" \
+  -o jabba "$BINARY_URL"
+chmod +x jabba
+
+# Install JDK
+
+./jabba install "${JABBA_JDK}"
+
+# Install JUnit
+
 MAVEN='https://repo1.maven.org/maven2'
 JUNIT_GROUP='org/junit/platform'
 JUNIT_ARTIFACT='junit-platform-console-standalone'
 
-ls -R # Debug output
-
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash
-source ~/.jabba/jabba.sh
-jabba install "${JABBA_JDK}"
-curl -sL "${MAVEN}/${JUNIT_GROUP}/${JUNIT_ARTIFACT}/${JUNIT_VERSION}/${JUNIT_ARTIFACT}-${JUNIT_VERSION}.jar" \
+curl -L "${MAVEN}/${JUNIT_GROUP}/${JUNIT_ARTIFACT}/${JUNIT_VERSION}/${JUNIT_ARTIFACT}-${JUNIT_VERSION}.jar" \
   -o junit.jar
-for class in $(jar tf z3-turnkey-*([0-9.])-integration-tests.jar | grep -F '.class' | sed 's#/#.#g;s#.class##g'); do
+
+# Run Tests
+
+for class in $(jar tf Build/z3-turnkey-*([0-9.])-integration-tests.jar |
+  grep -F '.class' | sed 's#/#.#g;s#.class##g'); do
   java -jar junit.jar \
-    --class-path z3-turnkey-*([0-9.]).jar \
-    --class-path z3-turnkey-*([0-9.])-integration-tests.jar \
+    --class-path Build/z3-turnkey-*([0-9.]).jar \
+    --class-path Build/z3-turnkey-*([0-9.])-integration-tests.jar \
     --reports-dir "test-$class" \
     --select-class "$class"
 done
