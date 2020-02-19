cea4369340146f7d3bca1fc778f13bbe4834fb5b
Simon Dierl <simon.dierl@cs.tu-dortmund.de>
2020-02-19 15:43:45 +0100

    externalize integration test run script

---
+++ contrib/run-integration-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+shopt -s extglob
+
+MAVEN='https://repo1.maven.org/maven2'
+JUNIT_GROUP='org/junit/platform'
+JUNIT_ARTIFACT='junit-platform-console-standalone'
+
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash
+source ~/.jabba/jabba.sh
+jabba install "${JABBA_JDK}"
+curl -sL "${MAVEN}/${JUNIT_GROUP}/${JUNIT_ARTIFACT}/${JUNIT_VERSION}/${JUNIT_ARTIFACT}-${JUNIT_VERSION}.jar" \
+  -o junit.jar
+for class in $(jar tf z3-turnkey-*([0-9.])-integration-tests.jar | grep -F '.class' | sed 's#/#.#g;s#.class##g'); do
+  java -jar junit.jar \
+    --class-path z3-turnkey-*([0-9.]).jar \
+    --class-path z3-turnkey-*([0-9.])-integration-tests.jar \
+    --reports-dir "test-$class" \
+    --select-class "$class"
+done
